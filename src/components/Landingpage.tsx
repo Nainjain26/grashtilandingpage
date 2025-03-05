b0daa41f6db7b5a81a67075b0e64d619ca7464ee
Nainjain26 <naina.jain2606@gmail.com>
2025-03-05 14:34:32 +0530

    update

--- src/components/Landingpage.tsx
+++ src/components/Landingpage.tsx
@@ -25,7 +25,7 @@ export default function Landingpage() {
         {showQR && (
           <div className="mt-6">
             <img
-              src="https://via.placeholder.com/200x200.png?text=QR+Code"
+              src="Qr.png"
               alt="QR Code"
               className="w-48 h-48 mx-auto"
             />
